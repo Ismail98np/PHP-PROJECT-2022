fefcadc51bc36cfe555634e3f1b9daf3183a346e
Ismail Omotoso <76391125+Ismail98np@users.noreply.github.com>
2022-04-27 08:07:33 +0100

    Skeleton cleanup

--- Crud_app/src/Controller/DefaultController.php
+++ Crud_app/src/Controller/DefaultController.php
@@ -5,14 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use App\Entity\County;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return new Response("<html><body>this webpage is working</body></html>");
     }
+    /** 
+    
+     * @Route("/save")
+     */
+    /*
+    public function save(ManagerRegistry $doctrine) :Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new County();
+        $product->setName('new one');
+
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response($product->getId());
+
+    }*/
+    
 }
